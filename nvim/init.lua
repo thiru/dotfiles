2ae37394f84cff250c337919998d2a8e9a020efd
Thiru Thirunavukarasu <thiru0130@gmail.com>
2022-09-18 21:41:34 -0400

    nvim - rename builtins.lua files to core.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,9 @@
 local doc = 'Config starting point.'
 
 require('thiru.plugins').setup()
-require('thiru.mappings.builtins').setup()
+require('thiru.mappings.core').setup()
 require('thiru.mappings.plugins').setup()
-require('thiru.settings.builtins').setup()
+require('thiru.settings.core').setup()
 require('thiru.settings.plugins').setup()
 
 return {
