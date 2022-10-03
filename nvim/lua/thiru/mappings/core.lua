db3f0bb9e15e707818ea5463a86da8aa6a9fb2d3
Thiru Thirunavukarasu <thiru0130@gmail.com>
2022-10-03 00:01:06 -0400

    nvim - bring over mappings

--- nvim/lua/thiru/mappings/core.lua
+++ nvim/lua/thiru/mappings/core.lua
@@ -1,22 +1,117 @@
 local doc = 'Key mappings for Neovim builtins (i.e. without plugins).'
 
-local function setup()
+local keymap = vim.keymap.set
+
+local function leaders()
   vim.g.mapleader = ' '
   vim.g.maplocalleader = ','
 
-  -- Keymaps for better default experience
-  -- See `:help vim.keymap.set()`
-  vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+  -- Disable a single spacebar key-press since we use it as the leader key
+  keymap({ 'n', 'v' }, '<Space>', '<Nop>', { desc = '', silent = true })
+end
 
-  -- Remap for dealing with word wrap
-  vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-  vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-  --
-  -- Diagnostic keymaps
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+local function config()
+  -- Open init.lua
+  keymap('n', '<leader>cc', ':e $MYVIMRC<CR>',
+         { desc = 'Open main Neovim config (init.lua)', silent = true })
+end
+
+local function buffers()
+  keymap('n', '<C-j>', ':bp<CR>', { desc = 'Previous buffer', silent = true })
+  keymap('n', '<C-k>', ':bn<CR>', { desc = 'Next buffer', silent = true })
+end
+
+local function diagnostics()
+  keymap('n', '[d', vim.diagnostic.goto_prev)
+  keymap('n', ']d', vim.diagnostic.goto_next)
+  keymap('n', '<leader>e', vim.diagnostic.open_float)
+end
+
+local function files()
+  -- CWD
+  keymap('n', '<leader>cd', ':cd %:p:h<CR>:pwd<CR>',
+         { desc = 'Change working directory to that of the current file' })
+
+  -- PWD
+  keymap('n', '<leader>wd', ':pwd<CR>', { desc = 'Print current working directory' })
+
+  -- Open file in clipboard
+  keymap('n', '<leader>fc', ':e <C-r>+<CR>',
+         { desc = 'Open file stored in system clipboard' })
+
+  -- Save file
+  keymap('n', '<leader>fs', ':w<CR>', { desc = 'Save current file' })
+  keymap('n', '<C-s>', ':w<CR>', { desc = 'Save current file' })
+  keymap('i', '<C-s>', '<C-o>:w<CR>', { desc = 'Save current file' })
+end
+
+local function misc()
+  -- Command-line up/down
+  keymap('c', '<C-j>', '<Down>', { desc = 'Next command (cmd-line mode)' })
+  keymap('c', '<C-k>', '<Up>', { desc = 'Previous command (cmd-line mode)' })
+
+  -- Execute current line in shell
+  keymap('n','<leader>x', ":exec '!'.getline('.')<CR>", { desc = 'Execute current line in shell' })
+
+  -- Quit
+  keymap({ 'n', 'v' }, '<leader>q', ':qall<CR>', { desc = 'Exit Vim (unless unsaved changes)' })
+  keymap({ 'n', 'v' }, '<leader>Q', ':qall!<CR>', { desc = 'Exit Vim (ignore unsaved changes)' })
+end
+
+local function terminal()
+  keymap('t', '<C-space>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+end
+
+local function text_navigation()
+  -- Centre screen on find next/previous
+  keymap('n', 'n', 'nzzzv', { desc = 'Go to next match and centre screen' })
+  keymap('n', 'N', 'Nzzzv', { desc = 'Go to previous match and centre screen' })
+
+  -- Diff mode next/previous change
+  if vim.opt.diff:get() then
+    keymap('n', '<S-J>', ']czz', { desc = 'Go to next change and centre (diff mode)' })
+    keymap('n', '<S-K>', '[czz', { desc = 'Go to previous change and centre (diff mode)' })
+  end
+
+  -- End of line
+  keymap('n', '<leader>l', '$', { desc = 'Go to end of line' })
+
+  -- Matching bracket
+  keymap('n','<TAB>', '%', { desc = 'Go to matching bracket, etc.' })
+
+  -- Navigate by screen lines, not logical lines (i.e. when lines are wrapped)
+  keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+  keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+  -- Search highlight off
+  keymap('n', '<leader>ho', ':nohlsearch<CR>', { desc = 'Search highlight off' })
+
+  -- Toggle word wrap
+  keymap('n', '<leader>ww', ':set wrap!<CR>', { desc = '' })
+end
+
+local function yanking()
+  -- Copy everything to the clipboard
+  keymap('n', '<leader>ya', ':%y+<CR>', { desc = 'Copy everything to system clipboard' })
+
+  -- Copy current file path to clipboard
+  keymap('n', '<leader>yf', ':let @+ = expand("%")<CR>',
+         { desc = 'Copy current file path to system clipboard' })
+
+  -- Paste from clipboard
+  keymap('i', '<C-v>', '<C-r>*', { desc = 'Paste from system clipboard' })
+end
+
+local function setup()
+  leaders()
+  buffers()
+  config()
+  diagnostics()
+  files()
+  misc()
+  terminal()
+  text_navigation()
+  yanking()
 end
 
 return {
--- nvim/lua/thiru/mappings/plugins.lua
+++ nvim/lua/thiru/mappings/plugins.lua
@@ -2,21 +2,30 @@ local doc = 'Key mappings for Neovim builtin actions.'
 
 -- See `:help telescope.builtin`
 local function telescope()
-  vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-  vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+  local builtin = require('telescope.builtin')
+  local themes = require('telescope.themes')
+
+  vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+  vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
   vim.keymap.set('n', '<leader>/', function()
     -- You can pass additional configuration to telescope to change theme, layout, etc.
-    require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    builtin.current_buffer_fuzzy_find(themes.get_dropdown {
       winblend = 10,
       previewer = false,
     })
   end, { desc = '[/] Fuzzily search in current buffer]' })
 
-  vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-  vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-  vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-  vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-  vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+  vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
+  vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
+  vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+  vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+  vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+  vim.keymap.set('n', '<leader>sh', builtin.command_history, { desc = '[S]earch [C]ommand [H]istory' })
+  vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps (normal mode)' })
+  vim.keymap.set('n', '<leader>so', builtin.oldfiles, { desc = '[S]earch [O]ld files' })
+  vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+  vim.keymap.set('n', '<leader>st', builtin.current_buffer_fuzzy_find, { desc = '[S]earch [T]ext in current buffer' })
+  vim.keymap.set('n', '<leader>sv', builtin.help_tags, { desc = '[S]earch [V]im help' })
 end
 
 local function setup()
