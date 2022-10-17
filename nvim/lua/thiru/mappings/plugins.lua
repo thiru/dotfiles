4b6dd18e216c02629b719a6ea0645c8b142f9132
Thiru Thirunavukarasu <thiru0130@gmail.com>
2022-10-16 20:53:05 -0400

    nvim - tweaked telescope bindings

--- nvim/lua/thiru/mappings/plugins.lua
+++ nvim/lua/thiru/mappings/plugins.lua
@@ -28,15 +28,15 @@ local function telescope()
   end, { desc = '[/] Fuzzily search in current buffer]' })
 
   vim.keymap.set('n', '<leader>sb', builtin.current_buffer_fuzzy_find, { desc = '[S]earch [T]ext in current buffer' })
-  vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
+  vim.keymap.set('n', '<leader>scc', builtin.commands, { desc = '[S]earch [C]ommands' })
+  vim.keymap.set('n', '<leader>sch', builtin.command_history, { desc = '[S]earch [C]ommand [H]istory' })
   vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
   vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
   vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-  vim.keymap.set('n', '<leader>sh', builtin.command_history, { desc = '[S]earch [C]ommand [H]istory' })
+  vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [V]im help' })
   vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps (normal mode)' })
   vim.keymap.set('n', '<leader>sm', builtin.marks, { desc = '[S]earch [M]arks' })
   vim.keymap.set('n', '<leader>so', builtin.oldfiles, { desc = '[S]earch [O]ld files' })
-  vim.keymap.set('n', '<leader>sv', builtin.help_tags, { desc = '[S]earch [V]im help' })
   vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 end
 
