f347be4075b16ca58a72b3a128d990e3cc4b4b25
Thiru Thirunavukarasu <thiru0130@gmail.com>
2022-10-20 17:47:21 -0400

    nvim - paste from clipboard from terminal mode

--- nvim/lua/thiru/mappings/core.lua
+++ nvim/lua/thiru/mappings/core.lua
@@ -107,6 +107,7 @@ local function yanking()
 
   -- Paste from clipboard
   keymap('i', '<C-v>', '<C-r>*', { desc = 'Paste from system clipboard' })
+  keymap('t', '<A-v>', '<C-\\><C-n>pi', { desc = 'Paste from system clipboard' })
 end
 
 local function setup()
