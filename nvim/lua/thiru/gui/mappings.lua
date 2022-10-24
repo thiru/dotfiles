252a65e3f7f4d1a4084c4580f9b03c1f0c1613bf
Thiru Thirunavukarasu <thiru0130@gmail.com>
2022-10-24 19:04:20 -0400

    nvim - increase GUI font in any mode

--- nvim/lua/thiru/gui/mappings.lua
+++ nvim/lua/thiru/gui/mappings.lua
@@ -3,11 +3,20 @@ local doc = 'Key mappings for Neovim GUIs'
 local gui_settings = require('thiru.gui.settings')
 
 local function setup()
-  vim.keymap.set('n', '<C-=>', '<cmd>:GUIFontSizeUp<CR>',
+
+  vim.keymap.set({'i', 'n', 't', 'v'},
+                 '<C-=>',
+                 '<cmd>:GUIFontSizeUp<CR>',
                  { desc = 'Increase GUI font size' })
-  vim.keymap.set('n', '<C-->', '<cmd>:GUIFontSizeDown<CR>',
+
+  vim.keymap.set({'i', 'n', 't', 'v'},
+                 '<C-->',
+                 '<cmd>:GUIFontSizeDown<CR>',
                  { desc = 'Decrease GUI font size' })
-  vim.keymap.set('n', '<C-0>', '<cmd>:GUIFontSizeSet ' .. gui_settings.font_size_default .. '<CR>',
+
+  vim.keymap.set({'i', 'n', 't', 'v'},
+                 '<C-0>',
+                 '<cmd>:GUIFontSizeSet ' .. gui_settings.font_size_default .. '<CR>',
                  { desc = 'Reset GUI font size to default value (' .. gui_settings.font_size_default .. ')' })
 end
 
