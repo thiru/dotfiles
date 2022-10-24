e11e1209a338b6f435df0b75536d616eba1a2e20
Thiru Thirunavukarasu <thiru0130@gmail.com>
2022-10-24 18:03:53 -0400

    nvim - neovide - use same transparency for plain term mode

--- nvim/lua/thiru/gui.lua
+++ nvim/lua/thiru/gui.lua
@@ -1,20 +1,12 @@
 local doc = 'Config for Neovim GUIs'
 
-local plain_term = require('thiru.plain-term')
-
 local function general()
   vim.opt.guifont = {'Fira Mono', ':h15'}
 end
 
 local function neovide()
-  if not vim.g.neovide then
-    return
-  end
-
-  if plain_term.is_enabled() then
-    vim.g.neovide_transparency = 0.80
-  else
-    vim.g.neovide_transparency = 0.95
+  if vim.g.neovide then
+    vim.g.neovide_transparency = 0.90
   end
 end
 
