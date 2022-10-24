799f38eb48dd48fb2e0d89e0ca72fe173837b0b4
Thiru Thirunavukarasu <thiru0130@gmail.com>
2022-10-24 18:59:09 -0400

    nvim - minor refactor around GUI font/size

--- nvim/lua/thiru/gui/settings.lua
+++ nvim/lua/thiru/gui/settings.lua
@@ -1,10 +1,13 @@
 local doc = 'Settings for Neovim GUIs'
 
+local font_size_default = 15
+
 local function setup()
-  vim.opt.guifont = {'Fira Mono', ':h15'}
+  vim.opt.guifont = {'Fira Mono', ':h' .. font_size_default}
 end
 
 return {
   doc = doc,
+  font_size_default = font_size_default,
   setup = setup
 }
