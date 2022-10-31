bcef2d36844174db232eb2db0709bb5813421bc6
Thiru Thirunavukarasu <thiru0130@gmail.com>
2022-10-31 17:13:51 -0400

    nvim - conjure - enable tree-sitter support

--- nvim/lua/thiru/settings/plugins.lua
+++ nvim/lua/thiru/settings/plugins.lua
@@ -72,6 +72,9 @@ local function conjure()
   vim.g['conjure#mapping#doc_word'] = {'<localleader>k'}
   vim.g['conjure#log#botright'] = true
 
+  -- Enable tree-sitter support:
+  vim.g['conjure#extract#tree_sitter#enabled'] = true
+
   -- Don't auto-start Babashka REPL
   vim.g['conjure#client#clojure#nrepl#connection#auto_repl#enabled'] = false
 
