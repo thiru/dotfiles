2e8cdf2d9d799dd3b8a9b71d6a300cfd0e5cfacb
Thiru Thirunavukarasu <thiru0130@gmail.com>
2023-09-23 23:11:01 -0400

    nvim - use transparent background in dark mode
    Since dark mode is really just used for terminal emulation.

--- nvim/lua/thiru/settings/plugins.lua
+++ nvim/lua/thiru/settings/plugins.lua
@@ -23,6 +23,7 @@ local function colour_scheme_dark()
   vim.g.edge_style = 'aura'
   vim.g.edge_better_performance = 1
   vim.g.edge_enable_italic = 1
+  vim.g.edge_transparent_background = 1
   vim.opt.background = 'dark'
   vim.cmd('colorscheme edge')
 end
