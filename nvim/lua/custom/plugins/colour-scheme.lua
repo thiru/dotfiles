62a19a812cf1270985b83d446cdc0270dc1fc588
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-03-08 20:47:25 -0500

    nvim - make some plugins load more lazily

--- nvim/lua/custom/plugins/colour-scheme.lua
+++ nvim/lua/custom/plugins/colour-scheme.lua
@@ -1,7 +1,8 @@
 return {
   'catppuccin/nvim',
   name = 'catppuccin',
-  priority = 1000,
+  lazy = false, -- make sure to load this during startup
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     require('catppuccin').setup({
       flavour = 'latte',
--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -2,6 +2,7 @@
 
 return {
   'nvim-telescope/telescope.nvim',
+  event = 'VimEnter',
   branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
