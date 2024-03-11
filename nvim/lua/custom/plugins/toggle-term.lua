c9e2eda25faa4bfb0b4c3c9ef2521949f0b1b015
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-03-11 00:53:01 -0400

    nvim - mapping to open terminal in CWD

--- nvim/lua/custom/plugins/toggle-term.lua
+++ nvim/lua/custom/plugins/toggle-term.lua
@@ -12,9 +12,23 @@ return {
         end
       end,
     })
-    vim.keymap.set('n', '<C-t>', ':ToggleTerm dir=git_dir<CR>', { desc = 'Open toggle-term terminal'})
     vim.keymap.set('n', '<leader>tl', ':ToggleTermSendCurrentLine<CR>', { desc = 'Send current line to terminal' })
     vim.keymap.set('v', '<leader>tl', ':ToggleTermSendVisualLines<CR>', { desc = 'Send visual lines to terminal' })
     vim.keymap.set('v', '<leader>ts', ':ToggleTermSendVisualSelection<CR>', { desc = 'Send visual selection to terminal' })
+
+
+    local Terminal  = require('toggleterm.terminal').Terminal
+    local cwd_term = Terminal:new({
+      on_create = function()
+        vim.cmd('sleep 300m') -- HACK: waiting for shell to be ready
+        vim.cmd('call chansend(b:terminal_job_id, "cd ' .. vim.loop.cwd() .. ' && clear \\<cr>")')
+      end
+    })
+
+    function CwdTermToggle()
+      cwd_term:toggle()
+    end
+
+    vim.api.nvim_set_keymap('n', '<C-t>', '<cmd>lua CwdTermToggle()<CR>', {desc = 'Open terminal in CWD', noremap = true, silent = true})
   end
 }
