a506e454da11d5937a26eed9194147dd0c7946d8
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-03-16 19:39:30 -0400

    wezterm - initial config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require('wezterm')
+
+local config = wezterm.config_builder()
+
+config.enable_scroll_bar = true
+config.font = wezterm.font('JetBrainsMono Nerd Font Mono')
+config.font_size = 15
+config.window_background_opacity = 0.9
+
+return config
