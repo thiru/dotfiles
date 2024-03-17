26af0bf221ebf56d0d8123dd12eb5a94722ac697
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-03-17 02:06:05 -0400

    wezterm - ui tweak

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,6 +5,7 @@ local config = wezterm.config_builder()
 config.enable_scroll_bar = true
 config.font = wezterm.font('JetBrainsMono Nerd Font Mono')
 config.font_size = 15
+config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.9
 
 return config
