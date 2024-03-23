0f6f310d9eab2cda597b6412fb00232b7901cc3f
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-03-22 23:33:21 -0400

    nvim - constrain floating oil window size

--- nvim/lua/custom/plugins/oil.lua
+++ nvim/lua/custom/plugins/oil.lua
@@ -3,7 +3,11 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require('oil').setup({
-      vim.keymap.set('n', '<leader>o', ':Oil --float .<CR>', { desc = 'Open Oil in floating window' })
+      float = {
+        max_width = 100,
+        max_height = 23
+      }
     })
+    vim.keymap.set('n', '<leader>o', ':Oil --float .<CR>', { desc = 'Open Oil in floating window' })
   end
 }
