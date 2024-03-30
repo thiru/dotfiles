05933c24e16ab59b0e4f9133b1a900f0fe9781b6
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-03-30 16:30:20 -0400

    nvim - dadbod - enable completion & better keybind for execute query

--- nvim/lua/custom/plugins/dadbod.lua
+++ nvim/lua/custom/plugins/dadbod.lua
@@ -12,10 +12,46 @@ return {
     'DBUIAddConnection',
     'DBUIFindBuffer',
   },
-  init = function()
+  keys = {
+    {'<leader><CR>',
+      function()
+        -- HACK: If we're on the last line with no new lines below then `Vip` fails and aborts the rest of the command
+        if vim.fn.line('.') == vim.fn.line('$') then
+          vim.api.nvim_input('V')
+        else
+          vim.api.nvim_input('Vip')
+        end
+
+        vim.api.nvim_input(':<Plug>(DBUI_ExecuteQuery)')
+      end,
+      mode = 'n', ft = 'sql', desc = 'Execute SQL query (paragraph)'},
+    {'<leader><CR>', '<Plug>(DBUI_ExecuteQuery)', mode = 'v', ft = 'sql', desc = 'Execute SQL query (selected)'}
+  },
+  config = function()
     vim.g.db_ui_execute_on_save = 0
     vim.g.db_ui_use_nerd_fonts = 1
     vim.g.db_ui_use_nvim_notify = 1
-    vim.keymap.set({'n', 'v'}, '<leader><CR>', '<Plug>(DBUI_ExecuteQuery)')
+
+    local function db_completion()
+      require("cmp").setup.buffer { sources = { { name = "vim-dadbod-completion" } } }
+    end
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "sql",
+      },
+      command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "sql",
+        "mysql",
+        "plsql",
+      },
+      callback = function()
+        vim.schedule(db_completion)
+      end,
+    })
   end,
 }
