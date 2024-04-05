1645a8106ff49ff45be99989f7243c8c51081a5f
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-04-04 21:30:37 -0400

    nvim - replace oil with mini.files

---
+++ nvim/lua/custom/plugins/mini-files.lua
@@ -0,0 +1,52 @@
+return {
+  "echasnovski/mini.files",
+  opts = {
+    windows = {
+      preview = false,
+    },
+    options = {
+      use_as_default_explorer = true,
+    },
+  },
+  keys = {
+    {
+      "<leader>o",
+      function()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+      end,
+      desc = "Open mini.files (Directory of Current File)",
+    },
+    {
+      "<leader>O",
+      function()
+        require("mini.files").open(vim.uv.cwd(), true)
+      end,
+      desc = "Open mini.files (cwd)",
+    },
+  },
+  config = function(_, opts)
+    require("mini.files").setup(opts)
+
+    local show_dotfiles = true
+    local filter_show = function()
+      return true
+    end
+    local filter_hide = function(fs_entry)
+      return not vim.startswith(fs_entry.name, ".")
+    end
+
+    local toggle_dotfiles = function()
+      show_dotfiles = not show_dotfiles
+      local new_filter = show_dotfiles and filter_show or filter_hide
+      require("mini.files").refresh({ content = { filter = new_filter } })
+    end
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesBufferCreate",
+      callback = function(args)
+        local buf_id = args.data.buf_id
+        vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id, desc = "Toggle Hidden Files" })
+      end,
+    })
+  end
+}
