4e1d334f64007e66f74f16572efa7260f3501bf9
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-04-07 13:06:53 -0400

    nvim - replace tagbar with aerieal

---
+++ nvim/lua/custom/plugins/aerial.lua
@@ -0,0 +1,16 @@
+return {
+  'stevearc/aerial.nvim',
+  dependencies = {
+     'nvim-treesitter/nvim-treesitter',
+     'nvim-tree/nvim-web-devicons'
+  },
+  opts = {
+    on_attach = function(bufnr)
+      vim.keymap.set('n', '{', '<CMD>AerialPrev<CR>', {buffer = bufnr, desc = 'Jump to previous symbol'})
+      vim.keymap.set('n', '}', '<CMD>AerialNext<CR>', {buffer = bufnr, desc = 'Jump to nextt symbol'})
+  end, },
+  keys = {
+    {'<leader>cn', '<CMD>AerialNavToggle<CR>', desc = 'Aerial Nav'},
+    {'<leader>cs', '<CMD>AerialToggle!<CR>', desc = 'Aerial Symbols'},
+  },
+}
--- nvim/lua/custom/plugins/tagbar.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'preservim/tagbar',
-  enabled = function()
-    return not vim.opt.diff:get()
-  end,
-  config = function()
-    vim.keymap.set('n', '<leader>ta', ':TagbarToggle<CR>', { desc = '' })
-  end
-}
