060010c2b0d02d7f3d2c3d75e57bb29edbbff79a
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-04-13 16:15:05 -0400

    nvim - new keymap to launch terminal & startinsert

--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -104,6 +104,9 @@ local function init()
   -- Terminal - ESC
   vim.keymap.set('t', '<C-space>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+  -- Terminal - new
+  vim.keymap.set('n', '<leader>t', ':term<CR>i',{ desc = 'Launch terminal' })
+
   -- Centre screen on find next/previous
   vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Go to next match and centre screen' })
   vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Go to previous match and centre screen' })
