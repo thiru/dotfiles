4339ccbc7d559c9832d1177d5c5796a1da98381d
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-04-13 15:15:56 -0400

    nvim - neogit plugin added

---
+++ nvim/lua/custom/plugins/neogit.lua
@@ -0,0 +1,15 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim", -- optional
+    "sindrets/diffview.nvim", -- optional - Diff integration
+  },
+  config = true,
+  keys = {
+    {'<leader>gs', ':Neogit<CR>', desc = 'Git status'},
+    {'<leader>gb', function () require('neogit').open({ 'branch' }) end, desc = 'Git branch'},
+    {'<leader>gl', function () require('neogit').open({ 'log' }) end, desc = 'Git log'},
+    {'<leader>gd', ':DiffviewOpen<CR>', desc = 'Git diff view'}
+  }
+}
