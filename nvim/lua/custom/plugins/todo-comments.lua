6f7395c98004381229cfe06f2d58c6ced508fa3b
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-04-13 19:08:18 -0400

    nvim - todo-comments - support Markdown files too
    - Don't only highlight in comments
    - Add highlight group to identify items being *waited* on

--- nvim/lua/custom/plugins/todo-comments.lua
+++ nvim/lua/custom/plugins/todo-comments.lua
@@ -2,5 +2,13 @@ return {
   'folke/todo-comments.nvim',
   event = 'VimEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = { signs = false }
+  opts = {
+    highlight = {
+      comments_only = false
+    },
+    keywords = {
+      WAIT = {color = 'warning', alt = {'WAITING', 'BLOCKED'}}
+    },
+    signs = false
+  }
 }
