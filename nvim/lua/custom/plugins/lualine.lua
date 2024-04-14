dc784ca86339541474b7b4cfebceb143e95fba98
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-04-13 20:13:04 -0400

    nvim - lualine tweaks around filename and parent dir

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -10,13 +10,13 @@ return {
     },
     sections = {
       lualine_a = {'mode'},
-      lualine_b = {'branch', 'diagnostics'},
-      lualine_c = {
-        function()
-          return vim.fn.getcwd()
-        end
+      lualine_b = {'branch'},
+      lualine_c = {'diagnostics'},
+      lualine_x = {
+        function() return vim.fn.getcwd() end,
+        'filename',
+        'filesize'
       },
-      lualine_x = {'filename', 'filesize'},
       lualine_y = {'progress'},
       lualine_z = {'location'}
     },
