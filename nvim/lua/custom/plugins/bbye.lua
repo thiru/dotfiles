a0122f65183d99e08dddbba33106465ccb5b343c
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-04-14 13:54:01 -0400

    nvim - bbye - silence keymaps

--- nvim/lua/custom/plugins/bbye.lua
+++ nvim/lua/custom/plugins/bbye.lua
@@ -2,7 +2,7 @@ return {
  'moll/vim-bbye',
   config = function()
     -- Close buffer without also closing splits
-    vim.keymap.set('n', '<leader>d', ':Bwipeout<CR>', { desc = 'Close buffer' })
-    vim.keymap.set('n', '<leader>D', ':Bwipeout!<CR>', { desc = 'Close buffer (even if modified)' })
+    vim.keymap.set('n', '<leader>d', ':Bwipeout<CR>', {desc = 'Close buffer', silent = true})
+    vim.keymap.set('n', '<leader>D', ':Bwipeout!<CR>', {desc = 'Close buffer (even if modified)', silent = true})
   end
 }
