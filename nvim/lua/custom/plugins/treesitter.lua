66efd788edfe64b3dda7530e2b21a7fc474f4de3
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-04-17 23:03:53 -0400

    nvim - follow kickstart sample more closely for treesitter

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -7,88 +7,45 @@ return {
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
     build = ':TSUpdate',
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = {
-          'bash',
-          'c',
-          'clojure',
-          'csv',
-          'css',
-          'dockerfile',
-          'gitignore',
-          'html',
-          'java',
-          'javascript',
-          'json',
-          'lua',
-          'make',
-          'markdown',
-          'markdown_inline',
-          'python',
-          'regex',
-          'sql',
-          'vim',
-          'vimdoc'
-        },
-
-        auto_install = false,
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = '<c-space>',
-            node_incremental = '<c-space>',
-            -- TODO: I'm not sure for this one.
-            scope_incremental = '<c-s>',
-            node_decremental = '<c-backspace>',
-          },
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              [']m'] = '@function.outer',
-              [']]'] = '@class.outer',
-            },
-            goto_next_end = {
-              [']M'] = '@function.outer',
-              [']['] = '@class.outer',
-            },
-            goto_previous_start = {
-              ['[m'] = '@function.outer',
-              ['[['] = '@class.outer',
-            },
-            goto_previous_end = {
-              ['[M'] = '@function.outer',
-              ['[]'] = '@class.outer',
-            },
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              ['<localleader>a'] = '@parameter.inner',
-            },
-            swap_previous = {
-              ['<localleader>A'] = '@parameter.inner',
-            },
-          },
-        },
-      }
+    opts = {
+      auto_install = false,
+      sync_install = false,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+      ensure_installed = {
+        'bash',
+        'c',
+        'clojure',
+        'csv',
+        'css',
+        'dockerfile',
+        'gitignore',
+        'html',
+        'java',
+        'javascript',
+        'json',
+        'lua',
+        'make',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'regex',
+        'sql',
+        'vim',
+        'vimdoc'
+      },
+    },
+    config = function(_, opts)
+      -- Prefer git instead of curl in order to improve connectivity in some environments
+      require('nvim-treesitter.install').prefer_git = true
+      ---@diagnostic disable-next-line: missing-fields
+      require('nvim-treesitter.configs').setup(opts)
     end
   }
 }
