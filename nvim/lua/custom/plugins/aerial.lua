e41bb48ffde213ec3a417079727861c7fa43eaba
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-04-18 23:03:37 -0400

    nvim - formatting

--- nvim/lua/custom/plugins/aerial.lua
+++ nvim/lua/custom/plugins/aerial.lua
@@ -8,7 +8,7 @@ return {
     on_attach = function(bufnr)
       vim.keymap.set('n', '{', '<CMD>AerialPrev<CR>', {buffer = bufnr, desc = 'Jump to previous symbol'})
       vim.keymap.set('n', '}', '<CMD>AerialNext<CR>', {buffer = bufnr, desc = 'Jump to nextt symbol'})
-  end, },
+    end, },
   keys = {
     {'<leader>cn', '<CMD>AerialNavToggle<CR>', desc = 'Aerial Nav'},
     {'<leader>cs', '<CMD>AerialToggle!<CR>', desc = 'Aerial Symbols'},
