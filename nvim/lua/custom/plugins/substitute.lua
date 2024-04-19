c4a2a146e561e1651b6bf53776e8865301757889
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-04-19 00:46:28 -0400

    nvim - substitute plugin added

---
+++ nvim/lua/custom/plugins/substitute.lua
@@ -0,0 +1,14 @@
+return {
+  'gbprod/substitute.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local substitute = require('substitute')
+
+    substitute.setup()
+
+    vim.keymap.set('n', '<leader>p', substitute.operator, { desc = 'Substitute with motion' })
+    vim.keymap.set('n', '<leader>pp', substitute.line, { desc = 'Substitute line' })
+    vim.keymap.set('n', '<leader>pP', substitute.eol, { desc = 'Substitute to end of line' })
+    vim.keymap.set('x', '<leader>p', substitute.visual, { desc = 'Substitute in visual mode' })
+  end,
+}
