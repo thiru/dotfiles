e378bb11f52b1e0b4c9f20e8e64c63dab4a78bd2
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-04-19 00:31:42 -0400

    nvim - lualine - show more file info

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -11,11 +11,19 @@ return {
     sections = {
       lualine_a = {'mode'},
       lualine_b = {'branch'},
-      lualine_c = {'diagnostics'},
+      lualine_c = {
+        function()
+          local abs_path = vim.api.nvim_buf_get_name(0)
+          local cwd = vim.fn.getcwd()
+          return string.sub(abs_path, #cwd + 2)
+        end,
+      },
       lualine_x = {
         function() return vim.fn.getcwd() end,
-        'filename',
-        'filesize'
+        'diagnostics',
+        'filesize',
+        'encoding',
+        'fileformat',
       },
       lualine_y = {'progress'},
       lualine_z = {'location'}
