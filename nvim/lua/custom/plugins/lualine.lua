71246987918cc3898d7cb1b1f01d4e9a6b2f8da2
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-04-20 14:02:18 -0400

    nvim - lualine - improved cwd/file path info

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -1,7 +1,42 @@
--- Status line implemented using lualine
+local function replace_home_with_tilde(path)
+  local home_dir = vim.loop.os_homedir() or ''
+  if vim.startswith(path, home_dir) then
+    return '~' .. string.sub(path, #home_dir + 1)
+  else
+    return path
+  end
+end
+
+local function get_cwd()
+  return ' ' .. replace_home_with_tilde(vim.fn.getcwd())
+end
+
+local function get_file_path()
+  local abs_path = vim.api.nvim_buf_get_name(0) or ''
+
+  if #abs_path == 0 then
+    return ''
+  end
+
+  local cwd = vim.fn.getcwd()
+  local final_path = ' '
+
+  if not vim.startswith(abs_path, cwd) then
+    final_path = final_path .. abs_path
+  else
+    final_path = final_path .. string.sub(abs_path, #cwd + 2)
+  end
+
+  return replace_home_with_tilde(final_path)
+end
+
 return {
   'nvim-lualine/lualine.nvim',
-  -- See `:help lualine.txt`
+  helpers = {
+    get_cwd = get_cwd,
+    get_file_path = get_file_path,
+    replace_home_with_tilde = replace_home_with_tilde,
+  },
   opts = {
     options = {
       icons_enabled = true,
@@ -13,21 +48,8 @@ return {
       lualine_b = {'branch'},
       lualine_c = {'diagnostics'},
       lualine_x = {
-        function()
-          local abs_path = vim.api.nvim_buf_get_name(0)
-          local cwd = vim.fn.getcwd()
-          local rel_path = string.sub(abs_path, #cwd + 2)
-          local home_dir = vim.loop.os_homedir() or ''
-          if vim.startswith(cwd, home_dir) then
-            cwd = '~' .. string.sub(cwd, #home_dir + 1)
-          end
-          cwd = ' ' .. cwd
-          if #rel_path > 0 then
-            return cwd .. " < " .. rel_path
-          else
-            return cwd
-          end
-        end,
+        get_cwd,
+        get_file_path,
         'filesize',
         'encoding',
         'fileformat',
