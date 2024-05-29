250ff7ca63de6df44937812330a2a5d848ed782a
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-05-28 23:12:31 -0400

    nvim - neodba - added bindings for most metadata commands

--- nvim/lua/custom/plugins/neodba.lua
+++ nvim/lua/custom/plugins/neodba.lua
@@ -15,5 +15,10 @@ return {
     {'<C-CR>', '<C-O><CMD>NeodbaExecSql<CR>', mode = 'i', ft = ft, desc = 'Neodba - Execute SQL'},
     {'<localleader>dm', '<CMD>NeodbaGetDatabaseInfo<CR>', mode = 'n', ft = ft, desc = 'Neodba - Get database info'},
     {'<localleader>dc', '<CMD>NeodbaGetColumnInfo<CR>', mode = {'n', 'v'}, ft = ft, desc = 'Neodba - Get column info'},
+    {'<localleader>ds', '<CMD>NeodbaGetSchemas<CR>', mode = {'n', 'v'}, ft = ft, desc = 'Neodba - Get all schemas'},
+    {'<localleader>dt', '<CMD>NeodbaGetTables<CR>', mode = {'n', 'v'}, ft = ft, desc = 'Neodba - Get all tables'},
+    {'<localleader>dv', '<CMD>NeodbaGetViews<CR>', mode = {'n', 'v'}, ft = ft, desc = 'Neodba - Get all views'},
+    {'<localleader>df', '<CMD>NeodbaGetFunctions<CR>', mode = {'n', 'v'}, ft = ft, desc = 'Neodba - Get all functions'},
+    {'<localleader>dp', '<CMD>NeodbaGetProcedures<CR>', mode = {'n', 'v'}, ft = ft, desc = 'Neodba - Get all procedures'},
   },
 }
