5d67ece2175b2042f42344aabd09f763f9e70c73
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-06-29 20:52:26 -0400

    nvim - which-key - mostly UI tweaks

--- nvim/lua/custom/plugins/which-key.lua
+++ nvim/lua/custom/plugins/which-key.lua
@@ -1,5 +1,21 @@
+local top_margin = (vim.fn.winheight(0) / 2) - 10
+
 return {
   'folke/which-key.nvim',
   event = 'VimEnter',
-  opts = {}
+  opts = {
+    window = {
+      border = 'single',
+      margin = {top_margin, 10, 0, 10},
+      position = 'top',
+      winblend = 20,
+    },
+    layout = {
+      align = 'center',
+    },
+    triggers_nowait = {
+      '`', -- marks
+      '<C-a>', -- nvtmux
+    }
+  }
 }
