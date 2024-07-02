a07adbfb2bc6b9539128cea6c8cda0dcb66ae930
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-07-01 21:09:53 -0400

    nvim - keymaps for tab navigation

--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -1,5 +1,27 @@
 local doc = 'Define key mappings.'
 
+local function go_to_tab(num)
+  local all_tabs = vim.fn.gettabinfo()
+  if #all_tabs > 1 then
+    if num <= #all_tabs then
+      if vim.fn.mode() == 't' then
+        local keys = vim.api.nvim_replace_termcodes('<C-\\><C-n>' .. all_tabs[num].tabnr .. 'gt<CR>i', true, true, true)
+        vim.api.nvim_feedkeys(keys, 'n', false)
+      else
+        vim.cmd('normal ' .. all_tabs[num].tabnr .. 'gt')
+      end
+    end
+  end
+end
+
+local function move_tab(dir)
+  if dir == 'left' then
+    vim.cmd('-tabmove')
+  else
+    vim.cmd('+tabmove')
+  end
+end
+
 local function init()
   -- Disable a single spacebar key-press since we use it as the leader key
   vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
@@ -91,6 +113,35 @@ local function init()
   if not nvtmux_auto_started() then
     vim.keymap.set('n', '<C-j>', '<CMD>bp<CR>', {desc = 'Previous buffer', silent = true})
     vim.keymap.set('n', '<C-k>', '<CMD>bn<CR>', {desc = 'Next buffer/tab', silent = true})
+  end
+
+  if not nvtmux_auto_started() then -- nvtmux already has these bindings
+    -- Tab nav - new
+    vim.keymap.set('n', '<C-t>', '<CMD>tabnew<CR>', {desc = 'New tab'})
+
+    -- Tab nav - last accessed
+    vim.keymap.set({'n', 'i'}, '<C-`>', '<CMD>:tabnext #<CR>', {desc = 'Go to last accessed tab', silent = true})
+
+    -- Tab nav - previous/next
+    vim.keymap.set({'n', 't'}, '<C-S-TAB>', '<CMD>tabprevious<CR>', {desc = 'Next tab', silent = true})
+    vim.keymap.set({'n', 't'}, '<C-TAB>', '<CMD>tabnext<CR>', {desc = 'Previous tab', silent = true})
+    vim.keymap.set({'n', 't'}, '<C-S-j>', '<CMD>tabprevious<CR>', {desc = 'Next tab', silent = true})
+    vim.keymap.set({'n', 't'}, '<C-S-k>', '<CMD>tabnext<CR>', {desc = 'Previous tab', silent = true})
+
+    -- Tab nav - go to index
+    for i=1,9 do
+      vim.keymap.set(
+        {'i', 'n', 't', 'v'},
+        '<C-' .. i .. '>',
+        function()
+          go_to_tab(i)
+        end,
+        {desc = 'Go to tab by index', silent=true})
+    end
+
+    -- Tab nav - move left/right
+    vim.keymap.set('n', '<C-,>', function() move_tab('left') end, {desc = 'Move tab to the left'})
+    vim.keymap.set('n', '<C-.>', function() move_tab('right') end, {desc = 'Move tab to the right'})
   end
 
   -- Window nav - left/down/up/right
