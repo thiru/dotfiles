6ada8709c275df49194a78749df48328c8f94daf
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-07-15 23:15:54 -0400

    nvim - which-key - fix deprecation warnings
    Seems like some previous capabilities were lost.

--- nvim/lua/custom/plugins/which-key.lua
+++ nvim/lua/custom/plugins/which-key.lua
@@ -1,21 +1,14 @@
-local top_margin = (vim.fn.winheight(0) / 2) - 5
-
 return {
   'folke/which-key.nvim',
   event = 'VimEnter',
   opts = {
-    window = {
-      border = 'single',
-      margin = {top_margin, 10, 0, 10},
-      position = 'top',
-      winblend = 20,
-    },
     layout = {
       align = 'center',
     },
-    triggers_nowait = {
-      '`', -- marks
-      '<C-a>', -- nvtmux
+    modes = {
+      defer = {
+        ['<C-a>'] = false, -- for nvtmux
+      }
     }
   }
 }
