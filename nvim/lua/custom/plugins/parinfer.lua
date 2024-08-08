364204e08330b769a2cd7cd4f8025c5119322d07
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-08-08 12:43:49 -0400

    nvim - parinfer-rust - disable on Windows

--- nvim/lua/custom/plugins/parinfer.lua
+++ nvim/lua/custom/plugins/parinfer.lua
@@ -1,5 +1,5 @@
 return {
   'eraserhd/parinfer-rust',
-  cond = not vim.opt.diff:get() and not nvtmux_auto_started(),
+  cond = not vim.opt.diff:get() and not nvtmux_auto_started() and not is_windows(),
   build = 'cargo build --release'
 }
