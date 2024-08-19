a7644bc75aca1160d96ca159a35b756d8abdceb5
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-08-18 23:44:20 -0400

    nvim - nvtmux - updates for telescope-ssh merger

--- nvim/lua/custom/plugins/nvtmux.lua
+++ nvim/lua/custom/plugins/nvtmux.lua
@@ -1,7 +1,18 @@
 return {
   'thiru/nvtmux.nvim',
+  cond = not vim.opt.diff:get() and nvtmux_auto_started(),
   dev = true,
+  depedencies = {'nvim-telescope/telescope.nvim'},
+  lazy = false,
   opts = {
-    colorscheme = 'catppuccin-mocha'
-  }
+    colorscheme = 'catppuccin-mocha',
+    ssh = {
+      auto_reconnect = {
+        when = 'always'
+      }
+    },
+  },
+  keys = {
+    {'<C-a>s', '<CMD>Ssh<CR>', desc = 'Open an [S]SH connection'},
+  },
 }
