2cb52e52b8f44759afc854f2020c91871aead717
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-12-08 16:53:46 -0500

    nvim - snacks - new plugin depcreating nvim-notify and vim-bbye

--- nvim/lua/custom/plugins/bbye.lua
+++
@@ -1,8 +0,0 @@
-return {
- 'moll/vim-bbye',
-  config = function()
-    -- Close buffer without also closing splits
-    vim.keymap.set('n', '<leader>d', ':Bwipeout<CR>', {desc = 'Close buffer', silent = true})
-    vim.keymap.set('n', '<leader>D', ':Bwipeout!<CR>', {desc = 'Close buffer (even if modified)', silent = true})
-  end
-}
---
+++ nvim/lua/custom/plugins/snacks.lua
@@ -0,0 +1,78 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = not nvtmux_auto_started() },
+    dashboard = { enabled = not nvtmux_auto_started() },
+    notifier = {
+      enabled = true,
+      timeout = 2000,
+    },
+    quickfile = { enabled = not nvtmux_auto_started() },
+    statuscolumn = { enabled = not nvtmux_auto_started() },
+    styles = {
+      notification = {
+        wo = { wrap = true } -- Wrap notifications
+      }
+    }
+  },
+  keys = {
+    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+    { "<leader>,",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+    { "<leader>d",  function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+    { "<leader>nh", function() Snacks.notifier.show_history() end, desc = "Notification History" },
+    { "<leader>nd", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+    { "<leader>fr", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+    { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse" },
+    { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
+    { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+    { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
+    {
+      "<leader>N",
+      desc = "Neovim News",
+      function()
+        Snacks.win({
+          file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+          width = 0.6,
+          height = 0.6,
+          wo = {
+            spell = false,
+            wrap = false,
+            signcolumn = "yes",
+            statuscolumn = " ",
+            conceallevel = 3,
+          },
+        })
+      end,
+    }
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VeryLazy",
+      callback = function()
+        -- Setup some globals for debugging (lazy-loaded)
+        _G.dd = function(...)
+          Snacks.debug.inspect(...)
+        end
+        _G.bt = function()
+          Snacks.debug.backtrace()
+        end
+        vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+        -- Create some toggle mappings
+        Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+        Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+        Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+        Snacks.toggle.diagnostics():map("<leader>ud")
+        Snacks.toggle.line_number():map("<leader>ul")
+        Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+        Snacks.toggle.treesitter():map("<leader>uT")
+        Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+        Snacks.toggle.inlay_hints():map("<leader>uh")
+      end,
+    })
+  end,
+}
