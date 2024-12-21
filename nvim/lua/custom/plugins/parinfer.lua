0e47cd0394784452082469b5cf247aa1a98a36a7
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-12-21 17:10:28 -0500

    nvim - util to check if a min version of glibc is present

--- nvim/lua/custom/plugins/parinfer.lua
+++ nvim/lua/custom/plugins/parinfer.lua
@@ -1,5 +1,5 @@
 return {
   'eraserhd/parinfer-rust',
-  cond = not vim.opt.diff:get() and not nvtmux_auto_started() and not is_windows(),
+  cond = not vim.opt.diff:get() and not nvtmux_auto_started() and not is_windows() and has_glibc_version(2,33),
   build = 'cargo build --release'
 }
--- nvim/lua/custom/utils.lua
+++ nvim/lua/custom/utils.lua
@@ -13,6 +13,28 @@ function _G.pp(...)
 end
 
 
+-- [[ Check whether at least the given version of glibc is present. ]]
+function _G.has_glibc_version(min_major, min_minor)
+  -- print('Requiring glibc: ' .. min_major .. '.' .. min_minor) -- DEBUG
+  local handle = io.popen('ldd --version')
+  local result = handle:read('*a')
+  handle:close()
+
+  local major, minor = result:match('(%d+)%.(%d+)')
+  major = tonumber(major)
+  minor = tonumber(minor)
+  -- print('Detected glibc version: ' .. major .. '.' .. minor) -- DEBUG
+
+  if major < min_major then
+    return false
+  elseif major == min_major then
+    return minor >= min_minor
+  end
+
+  return true
+end
+
+
 function _G.nvtmux_auto_started()
   return vim.g.nvtmux_auto_start == true
 end
