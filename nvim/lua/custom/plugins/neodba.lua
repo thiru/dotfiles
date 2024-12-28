469324735fcad32f90496847c6b1ac9f111451d9
Thiru Thirunavukarasu <thiru0130@gmail.com>
2024-12-27 21:38:27 -0500

    nvim - neodba - fix table rendering issue

--- nvim/lua/custom/plugins/neodba.lua
+++ nvim/lua/custom/plugins/neodba.lua
@@ -4,6 +4,11 @@ return {
   'thiru/neodba.nvim',
   cond = not vim.opt.diff:get() and not u.nvtmux_auto_started(),
   dev = true,
-  config = true,
   ft = 'sql',
+  opts = {
+    -- HACK: these hooks are used to work around the RenderMarkdown plugin failing to correctly
+    -- render the output table after being updated.
+    pre_hook = function() vim.cmd('RenderMarkdown disable') end,
+    post_hook = function() vim.cmd('RenderMarkdown enable') end,
+  },
 }
