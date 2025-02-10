4b593436dee4ecdec51af147641d29fc2c3b1984
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-02-09 21:23:12 -0500

    nvim - snacks - replaced most Telescope pickers with snacks

--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -61,27 +61,7 @@ return {
     local builtin = require('telescope.builtin')
     local themes = require('telescope.themes')
 
-    vim.keymap.set('n', '<leader>s/',
-                   function()
-                     builtin.current_buffer_fuzzy_find(themes.get_dropdown({
-                       winblend = 10,
-                       previewer = false,
-                     }))
-                   end,
-                   { desc = '[/] Fuzzily search in current buffer]' })
-
-    vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
     vim.keymap.set('n', '<leader>se', builtin.symbols, { desc = '[S]earch [E]mojis' })
     vim.keymap.set('n', '<C-g>',      '<CMD>Telescope cder<CR>', { desc = '[S]earch and change to [D]irectory' })
-    vim.keymap.set('n', '<leader>sD', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [V]im help' })
-    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps (normal mode)' })
-    vim.keymap.set('n', '<leader>sm', builtin.marks, { desc = '[S]earch [M]arks' })
-    vim.keymap.set('n', '<leader>so', builtin.oldfiles, { desc = '[S]earch [O]ld files' })
-    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sx', builtin.command_history, { desc = '[S]earch Command History [X]' })
   end
 }
