677ce416ee7ad7e113f1e9a4a6a2239c277f3550
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-03-01 22:28:17 -0500

    rooter - prevent lazy loading

--- nvim/lua/custom/plugins/rooter.lua
+++ nvim/lua/custom/plugins/rooter.lua
@@ -3,6 +3,7 @@ local u = require('custom.utils')
 return {
   'airblade/vim-rooter',
   cond = not vim.opt.diff:get() and not u.nvtmux_auto_started(),
+  lazy = false,
   config = function()
     vim.g.rooter_patterns = {'.git', '.stfolder'}
   end
