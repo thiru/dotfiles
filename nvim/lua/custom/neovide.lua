a60f177afcc7a26968848b70c158399c398d3783
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-04-03 18:59:44 -0400

    nvim - fix neovide transparency -> opacity breaking change

--- nvim/lua/custom/neovide.lua
+++ nvim/lua/custom/neovide.lua
@@ -1,38 +1,38 @@
 local u = require('custom.utils')
 local doc = 'Neovide-specific configs'
 
-local transparency_default = 1.0
-local transparency_step = 0.05
+local opacity_default = 1.0
+local opacity_step = 0.05
 local term_trans_override = 0.95
 if u.is_windows() then
   term_trans_override = 1.0
 end
 
-local function transparency_print()
-  print('Neovide transparency = ' .. vim.g.neovide_transparency)
+local function opacity_print()
+  print('Neovide opacity = ' .. vim.g.neovide_opacity)
 end
 
-local function set_default_transparency()
+local function set_default_opacity()
   if u.nvtmux_auto_started() then
-    vim.g.neovide_transparency = term_trans_override
+    vim.g.neovide_opacity = term_trans_override
   else
-    vim.g.neovide_transparency = transparency_default
+    vim.g.neovide_opacity = opacity_default
   end
 end
 
-local function transparency_reset()
-  set_default_transparency()
-  transparency_print()
+local function opacity_reset()
+  set_default_opacity()
+  opacity_print()
 end
 
-local function transparency_inc()
-  vim.g.neovide_transparency = math.min(vim.g.neovide_transparency + transparency_step, 1)
-  transparency_print()
+local function opacity_inc()
+  vim.g.neovide_opacity = math.min(vim.g.neovide_opacity + opacity_step, 1)
+  opacity_print()
 end
 
-local function transparency_dec()
-  vim.g.neovide_transparency = math.max(vim.g.neovide_transparency - transparency_step, 0)
-  transparency_print()
+local function opacity_dec()
+  vim.g.neovide_opacity = math.max(vim.g.neovide_opacity - opacity_step, 0)
+  opacity_print()
 end
 
 local function init()
@@ -42,37 +42,37 @@ local function init()
 
   vim.cmd('silent exe "cd ~"')
   vim.g.neovide_hide_mouse_when_typing = true
-  set_default_transparency()
+  set_default_opacity()
 
   vim.api.nvim_create_user_command(
-    'NeovideTransparencyDec',
-    transparency_dec,
-    {bang = true, desc = 'Decrement Neovide transparency'}
+    'NeovideOpacityDec',
+    opacity_dec,
+    {bang = true, desc = 'Decrement Neovide opacity'}
   )
 
   vim.api.nvim_create_user_command(
-    'NeovideTransparencyInc',
-    transparency_inc,
-    {bang = true, desc = 'Increment Neovide transparency'}
+    'NeovideOpacityInc',
+    opacity_inc,
+    {bang = true, desc = 'Increment Neovide opacity'}
   )
 
   vim.api.nvim_create_user_command(
-    'NeovideTransparencyReset',
-    transparency_reset,
-    {bang = true, desc = 'Reset Neovide transparency to default setting'}
+    'NeovideOpacityReset',
+    opacity_reset,
+    {bang = true, desc = 'Reset Neovide opacity to default setting'}
   )
 
-  vim.keymap.set({'i', 'n', 't', 'v'}, '<A-=>', '<cmd>:NeovideTransparencyInc<CR>')
-  vim.keymap.set({'i', 'n', 't', 'v'}, '<A-->', '<cmd>:NeovideTransparencyDec<CR>')
-  vim.keymap.set({'i', 'n', 't', 'v'}, '<A-0>', '<cmd>:NeovideTransparencyReset<CR>')
+  vim.keymap.set({'i', 'n', 't', 'v'}, '<A-=>', '<cmd>:NeovideOpacityInc<CR>')
+  vim.keymap.set({'i', 'n', 't', 'v'}, '<A-->', '<cmd>:NeovideOpacityDec<CR>')
+  vim.keymap.set({'i', 'n', 't', 'v'}, '<A-0>', '<cmd>:NeovideOpacityReset<CR>')
 end
 
 return {
   doc = doc,
   init = init,
-  transparency_inc = transparency_inc,
-  transparency_default = transparency_default,
-  transparency_dec = transparency_dec,
-  transparency_print = transparency_print,
-  transparency_reset = transparency_reset,
+  opacity_inc = opacity_inc,
+  opacity_default = opacity_default,
+  opacity_dec = opacity_dec,
+  opacity_print = opacity_print,
+  opacity_reset = opacity_reset,
 }
