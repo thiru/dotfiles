e436741af8d372c966b05da5080047db43da5b9f
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-04-21 18:23:03 -0400

    nvim - neoscroll - removed

--- .config/nvim/lua/custom/plugins/neoscroll.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'karb94/neoscroll.nvim',
-  cond = not vim.g.neovide,
-  opts = {
-    duration_multiplier = 0.5,
-    easing = 'quadratic',
-    mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zz'},
-  },
-}
