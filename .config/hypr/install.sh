121770a36114ec7822ded96b8dbee25ae6fc942e
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-05-02 16:23:47 -0400

    hyprland - install script

---
+++ .config/hypr/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+pacman --needed -S \
+  brightnessctl \
+  grim \
+  qt5-wayland \
+  qt6-wayland \
+  slurp \
+  hypridle \
+  hyprland \
+  waybar \
+  wf-recorder \
+  ydotool \
+  xorg-xwayland \
+  xdg-desktop-portal-hyprland
+
