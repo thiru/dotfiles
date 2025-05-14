48fbfd542be7d01c5dea7f7b2e60d0ac4a32d7cd
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-05-14 16:00:33 -0400

    hyprland - many more config changes
    - vim motion submaps
    - groups/tabs submap
    - screenshot & screen record menu
    - power menu improvements/fixes
    - Hyprland-specific idle detection
    - zoom in/out
    - remove ydotoold (Hyprland has mouse simulation built in)
    - reduce outer gaps
    - and other changes

---
+++ scripts/printscreen-menu.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+export TERMINAL=kitty
+export PROVIDERS_FILE=$HOME/.config/sway-launcher-desktop/printscreen-provider.conf
+
+SCREENSHOTS_DIR=~/Pictures/screen-shots
+SCREENRECORDS_DIR=~/Videos/screen-recordings
+
+case "$1" in
+  "screenshot-screen-file")
+    sleep 1s
+    grim $SCREENSHOTS_DIR/screen-$(date +'%Y-%m-%dT%H-%M-%S').png && notify-send "Screenshot saved to $SCREENSHOTS_DIR"
+    ;;
+  "screenshot-screen-clipboard")
+    sleep 1s
+    grim - | wl-copy && notify-send "Screenshot saved to clipboard"
+    ;;
+  "screenshot-region-file")
+    grim -g "$(slurp)" $SCREENSHOTS_DIR/region-$(date +'%Y-%m-%dT%H-%M-%S').png && notify-send "Screenshot saved to $SCREENSHOTS_DIR"
+    ;;
+  "screenshot-region-clipboard")
+    grim -g "$(slurp)" - | wl-copy && notify-send "Screenshot saved to clipboard"
+    ;;
+  "record-screen-file")
+    DELAY=3s
+    notify-send "Recording in $DELAY..."
+    sleep $DELAY
+    kitty --title 'Screen Recording' --app-id launcher wf-recorder --file=$SCREENRECORDS_DIR/screen-$(date +'%Y-%m-%dT%H-%M-%S').mp4 && notify-send "Recording saved to $SCREENRECORDS_DIR"
+    ;;
+  "record-region-file")
+    kitty --title 'Screen Recording' --app-id launcher wf-recorder -g "$(slurp)" --file=$SCREENRECORDS_DIR/region-$(date +'%Y-%m-%dT%H-%M-%S').mp4 && notify-send "Recording saved to $SCREENRECORDS_DIR"
+    ;;
+  *)
+    kitty --title Launcher --app-id launcher --override font_size=20 sway-launcher-desktop
+    ;;
+esac
