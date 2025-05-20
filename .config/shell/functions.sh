5474f927f55190473b392cbf23ffe326ba62acfc
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-05-20 18:14:19 -0400

    yazi - initial config

--- .config/shell/functions.sh
+++ .config/shell/functions.sh
@@ -33,3 +33,11 @@ cdfzf() {
   cd "$dir" || echo 'aborted'
 }
 
+# Change current directory with yazi
+function y() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd < "$tmp"
+  [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+  rm -f -- "$tmp"
+}
