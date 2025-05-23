b0245286f3f11750e9902a8e37e08861f4ba2198
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-05-23 17:00:29 -0400

    nvim - conjure - specify main branch
    This was recommended by the plugin although setting the branch in lazy
    didn't seem to work. I had to switch branches manually.

--- .config/nvim/lua/custom/plugins/conjure.lua
+++ .config/nvim/lua/custom/plugins/conjure.lua
@@ -3,6 +3,7 @@ local u = require('custom.utils')
 return {
   'Olical/conjure',
   cond = not vim.opt.diff:get() and not u.nvtmux_auto_started(),
+  branch = 'main',
   config = function()
     vim.g['conjure#mapping#doc_word'] = {'<localleader>k'}
     vim.g['conjure#log#botright'] = true
