b0b6dcac8bfee994270ea4e01217a8515bf4e4ba
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-05-25 00:09:04 -0400

    shell - remove unused aliases

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -2,17 +2,14 @@
 
 curr_shell=`ps -p $$ | tail -1 | awk '{print $NF}'`
 
-alias dn='dotnet'
 alias g='git'
 alias nv='neovide'
-alias rename-vim='vidir'
 
 if command -v exa &> /dev/null; then
   alias l='exa -l'
   alias lg='exa -l --git'
   alias ll='exa -la'
   alias lt='exa --tree'
-  alias lw='exa'
 else
   alias l='ls -l'
   alias ll='ls -la'
@@ -22,13 +19,9 @@ alias sc='systemctl'
 
 if command -v nvim &> /dev/null; then
   alias v='nvim'
-  alias vterm='nvim --cmd "lua vim.g.plain_term = true"'
 else
   alias v='vim'
 fi
-
-alias disks='lsblk -o +LABEL,PARTLABEL,UUID,PARTUUID'
-alias assh='TERM=xterm-256color ssh'
 
 if [ $curr_shell = "zsh" ]; then
   compdef g=git
@@ -42,4 +35,3 @@ fi
 
 # The following will allow sudo to find aliases
 alias sudo='sudo '
-
