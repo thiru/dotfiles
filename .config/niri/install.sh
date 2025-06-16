a5027a7656b45f66642350b766de504cdbcd2ef1
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-06-16 00:22:00 -0400

    WIP niri - updated config based on Hyprland

---
+++ .config/niri/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+pacman --needed -S \
+  brightnessctl \
+  niri \
+  nwg-look \
+  swayidle \
+  waybar \
+  xdg-desktop-portal-gnome \
+  xdg-desktop-portal-gtk \
+  xwayland-satellite \
+  ydotool
