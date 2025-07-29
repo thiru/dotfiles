c68ce1e0702590894e6cca35473a21e03e855a15
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-07-29 19:19:59 -0400

    Use Maple Mono nerd font with no ligature and Chinese support

--- .config/nvim/lua/custom/options.lua
+++ .config/nvim/lua/custom/options.lua
@@ -59,7 +59,7 @@ local function init()
   vim.g.maplocalleader = ','
 
   -- Set font for GUI front-ends (like Neovide)
-  vim.opt.guifont = {'Maple Mono NF', ':h' .. font_size_default}
+  vim.opt.guifont = {'Maple Mono Normal NL NF CN', ':h' .. font_size_default}
 
   -- Disable backup and recovery files
   vim.o.backup = false
