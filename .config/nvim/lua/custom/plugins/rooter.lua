0584dc9295ee3721c34055d454f36eb48b59653a
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-07-31 22:19:56 -0400

    nvim - facilitate forced root dir

--- .config/nvim/lua/custom/plugins/rooter.lua
+++ .config/nvim/lua/custom/plugins/rooter.lua
@@ -5,6 +5,6 @@ return {
   cond = not vim.opt.diff:get() and not u.nvtmux_auto_started(),
   lazy = false,
   config = function()
-    vim.g.rooter_patterns = {'deps.edn', '.git', '.stfolder'}
+    vim.g.rooter_patterns = {'deps.edn', '.git', '.stfolder', '.forced-root'}
   end
 }
