11b7c77936b62eec9f9c9aff7e221c1a9b8561da
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-08-16 20:57:17 -0400

    nvim - replace parinfer-rust with nvim-parinfer

--- .config/nvim/lua/custom/plugins/parinfer.lua
+++ .config/nvim/lua/custom/plugins/parinfer.lua
@@ -1,7 +1,6 @@
 local u = require('custom.utils')
 
 return {
-  'eraserhd/parinfer-rust',
-  cond = not vim.opt.diff:get() and not u.nvtmux_auto_started() and not u.is_windows() and u.has_glibc_version(2,33),
-  build = 'cargo build --release'
+  'gpanders/nvim-parinfer',
+  cond = not vim.opt.diff:get() and not u.nvtmux_auto_started()
 }
