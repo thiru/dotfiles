c150ae2cc645551117a9ebe7500e07c0d51b17d8
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-09-22 19:58:59 -0400

    nvim - nvim-cmp - use default mappings for next/prev item

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -40,11 +40,6 @@ return { -- Autocompletion
       --
       -- No, but seriously. Please read `:help ins-completion`, it is really good!
       mapping = cmp.mapping.preset.insert({
-        -- Select the [n]ext item
-        ['<C-j>'] = cmp.mapping.select_next_item(),
-        -- Select the [p]revious item
-        ['<C-k>'] = cmp.mapping.select_prev_item(),
-
         -- Scroll the documentation window [b]ack / [f]orward
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
