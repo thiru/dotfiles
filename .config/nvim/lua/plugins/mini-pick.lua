40f28877ff1fbfb9cdf8b0b10e2adaf1b592d9f9
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-10-01 23:38:06 -0400

    nvim - mini-pick - use default mappings for next/prev item

--- .config/nvim/lua/plugins/mini-pick.lua
+++ .config/nvim/lua/plugins/mini-pick.lua
@@ -5,10 +5,7 @@ return {
     mappings = {
       choose_marked = '<C-CR>',
       mark = '<Tab>',
-      move_down = '<C-j>',
       move_start = '<C-g>',
-      move_up = '<C-k>',
-      toggle_preview = '<C-P>',
     }
   },
 }
