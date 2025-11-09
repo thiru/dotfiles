9ed8070cc007f81c72cb4eb5b7bae40cd362cff4
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-11-09 14:46:51 -0500

    nvim - nvtmux - remove obsolete dependency on taboo

--- .config/nvim/lua/plugins/nvtmux.lua
+++ .config/nvim/lua/plugins/nvtmux.lua
@@ -4,9 +4,7 @@ return {
   'thiru/nvtmux.nvim',
   cond = not vim.opt.diff:get() and u.nvtmux_auto_started(),
   dev = true,
-  depedencies = {
-    'gcmt/taboo.vim',
-    'nvim-telescope/telescope.nvim'},
+  depedencies = {'nvim-telescope/telescope.nvim'},
   opts = {
     colorscheme = 'catppuccin-mocha',
   },
