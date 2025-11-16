3acc14e3d5f815cbefc0760922b0b4ea455debfc
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-11-16 02:01:43 -0500

    nvim - nvtmux - adjust Neovide opacity on tab change

--- .config/nvim/lua/config/neovide.lua
+++ .config/nvim/lua/config/neovide.lua
@@ -1,69 +1,70 @@
 local u = require('config.utils')
-local doc = 'Neovide-specific configs'
 
-local opacity_default = 1.0
-local opacity_step = 0.05
-local terminal_opacity_override = u.is_windows() and 1.0 or 0.95
-local scale_factor_delta_default = 1.1
+local M = {
+  opacity_default = 1.0,
+  opacity_step = 0.05,
+  terminal_opacity_override = u.is_windows() and 1.0 or 0.95,
+  scale_factor_delta_default = 1.1,
+}
 
-local function opacity_print()
+function M.opacity_print()
   print('Neovide opacity = ' .. vim.g.neovide_opacity)
 end
 
-local function set_default_opacity()
+function M.set_default_opacity()
   if u.nvtmux_auto_started() then
-    vim.g.neovide_opacity = terminal_opacity_override
+    vim.g.neovide_opacity = M.terminal_opacity_override
   else
-    vim.g.neovide_opacity = opacity_default
+    vim.g.neovide_opacity = M.opacity_default
   end
 end
 
-local function opacity_reset()
-  set_default_opacity()
-  opacity_print()
+function M.opacity_reset()
+  M.set_default_opacity()
+  M.opacity_print()
 end
 
-local function opacity_inc()
-  vim.g.neovide_opacity = math.min(vim.g.neovide_opacity + opacity_step, 1)
-  opacity_print()
+function M.opacity_inc()
+  vim.g.neovide_opacity = math.min(vim.g.neovide_opacity + M.opacity_step, 1)
+  M.opacity_print()
 end
 
-local function opacity_dec()
-  vim.g.neovide_opacity = math.max(vim.g.neovide_opacity - opacity_step, 0)
-  opacity_print()
+function M.opacity_dec()
+  vim.g.neovide_opacity = math.max(vim.g.neovide_opacity - M.opacity_step, 0)
+  M.opacity_print()
 end
 
-local function scale_factor_update(delta)
+function M.scale_factor_update(delta)
   vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 end
 
-local function scale_factor_reset()
+function M.scale_factor_reset()
   vim.g.neovide_scale_factor = 1.0
 end
 
-local function init()
+function M.init()
   if vim.g.neovide then
     vim.cmd('silent exe "cd ~"')
     vim.g.neovide_hide_mouse_when_typing = true
     vim.g.neovide_scale_factor = 1.0
-    set_default_opacity()
+    M.set_default_opacity()
   end
 
   vim.api.nvim_create_user_command(
     'NeovideOpacityDec',
-    opacity_dec,
+    M.opacity_dec,
     {bang = true, desc = 'Decrement Neovide opacity'}
   )
 
   vim.api.nvim_create_user_command(
     'NeovideOpacityInc',
-    opacity_inc,
+    M.opacity_inc,
     {bang = true, desc = 'Increment Neovide opacity'}
   )
 
   vim.api.nvim_create_user_command(
     'NeovideOpacityReset',
-    opacity_reset,
+    M.opacity_reset,
     {bang = true, desc = 'Reset Neovide opacity to default setting'}
   )
 
@@ -73,17 +74,9 @@ local function init()
   vim.keymap.set({'i', 'n', 't', 'v'}, '<A-0>', '<cmd>:NeovideOpacityReset<CR>', {desc='Reset opacity'})
 
   -- Scale Factor
-  vim.keymap.set({'i', 'n', 't', 'v'}, '<C-=>', function() scale_factor_update(scale_factor_delta_default) end, {desc='Increase scale factor'})
-  vim.keymap.set({'i', 'n', 't', 'v'}, '<C-->', function() scale_factor_update(1 / scale_factor_delta_default) end, {desc='Decrease scale factor'})
-  vim.keymap.set({'i', 'n', 't', 'v'}, '<C-0>', scale_factor_reset, {desc='Reset scale factor'})
+  vim.keymap.set({'i', 'n', 't', 'v'}, '<C-=>', function() M.scale_factor_update(M.scale_factor_delta_default) end, {desc='Increase scale factor'})
+  vim.keymap.set({'i', 'n', 't', 'v'}, '<C-->', function() M.scale_factor_update(1 / M.scale_factor_delta_default) end, {desc='Decrease scale factor'})
+  vim.keymap.set({'i', 'n', 't', 'v'}, '<C-0>', M.scale_factor_reset, {desc='Reset scale factor'})
 end
 
-return {
-  doc = doc,
-  init = init,
-  opacity_inc = opacity_inc,
-  opacity_default = opacity_default,
-  opacity_dec = opacity_dec,
-  opacity_print = opacity_print,
-  opacity_reset = opacity_reset,
-}
+return M
--- .config/nvim/lua/plugins/nvtmux.lua
+++ .config/nvim/lua/plugins/nvtmux.lua
@@ -1,4 +1,5 @@
 local u = require('config.utils')
+local neovide = require('config.neovide')
 
 return {
   'thiru/nvtmux.nvim',
@@ -8,5 +9,18 @@ return {
   opts = {
     auto_start = u.nvtmux_auto_started(),
     colorscheme = 'catppuccin-mocha',
+    on_tab_changed = function(is_term_tab)
+      if vim.g.neovide then
+        if is_term_tab then
+          if vim.g.neovide_opacity ~= neovide.terminal_opacity_override then
+            vim.g.neovide_opacity = neovide.terminal_opacity_override
+          end
+        else
+          if vim.g.neovide_opacity ~= neovide.opacity_default then
+            vim.g.neovide_opacity = neovide.opacity_default
+          end
+        end
+      end
+    end
   },
 }
