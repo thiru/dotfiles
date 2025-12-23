946d2294b60cb3f54d4d90c8a9522837faea0fbe
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-12-23 15:09:34 -0500

    nvim - mini-pick - make keymaps consistent with existing analagous ones

--- .config/nvim/lua/plugins/mini-pick.lua
+++ .config/nvim/lua/plugins/mini-pick.lua
@@ -6,7 +6,10 @@ return {
     mappings = {
       choose_marked = '<C-CR>',
       mark = '<Tab>',
+      move_down = '<C-j>',
       move_start = '<C-g>',
+      move_up = '<C-k>',
+      paste = '<C-v>',
     }
   },
 }
