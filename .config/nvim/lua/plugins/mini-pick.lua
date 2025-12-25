fcbcfd1dc12f884bb0f5a266604d865bf74ad014
Thiru Thirunavukarasu <thiru0130@gmail.com>
2025-12-24 21:38:44 -0500

    nvim - mini-pick - replacing snacks with this more

--- .config/nvim/lua/plugins/mini-pick.lua
+++ .config/nvim/lua/plugins/mini-pick.lua
@@ -1,3 +1,13 @@
+---@module 'mini.pick'
+
+local u = require('config.utils')
+
+local fd = {'fd', '--exclude', '.git',
+                  '--follow',
+                  '--hidden',
+                  '--type', 'file',
+                  '--type', 'symlink'}
+
 return {
   'nvim-mini/mini.pick',
   cond = not vim.opt.diff:get(),
@@ -10,6 +20,33 @@ return {
       move_start = '<C-g>',
       move_up = '<C-k>',
       paste = '<C-v>',
+      toggle_preview = '<C-Tab>',
     }
   },
+  keys = {
+    { '<leader>sb', function() MiniPick.builtin.buffers() end, desc = 'Search Buffers' },
+    { '<leader>sf', function() MiniPick.builtin.cli({command=fd}) end, desc = 'Search Files' },
+    { '<leader>sh', function() MiniPick.builtin.help() end, desc = 'Search Help' },
+    { '<leader>sR', function() MiniPick.builtin.resume() end, desc = 'Resume latest picker' },
+    { '<leader>st', function() MiniPick.builtin.grep_live() end, desc = 'Search Text (live)' },
+    { '<leader>sv',
+      function()
+        MiniPick.builtin.cli({command = fd},
+                             {source = {cwd = vim.fn.stdpath("config")}})
+      end,
+      desc = 'Search Neovim Configs' },
+    { '<leader>sw',
+      function()
+        local mode = vim.fn.mode()
+        local text = ''
+        if mode == 'v' or mode == 'V' then
+            text = u.selected_text()
+        else
+            text = vim.fn.expand('<cword>')
+        end
+        MiniPick.builtin.grep({pattern=text})
+      end,
+      mode = {'n','v'},
+      desc = 'Search Word/Highlight' },
+  }
 }
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -17,52 +17,10 @@ return {
     statuscolumn = {},
   },
   keys = {
-    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
-    { "<leader>,",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
     { "<leader>d",  function() Snacks.bufdelete() end, desc = "Delete Buffer" },
     { "<leader>D",  function() Snacks.bufdelete({force = true}) end, desc = "Delete Buffer (even if modified)" },
     { "<leader>nh", function() Snacks.notifier.show_history() end, desc = "Notification History" },
     { "<leader>nd", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-    { "<leader>fr", function() Snacks.rename.rename_file() end, desc = "Rename File" },
-    { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
-    { "<leader>gB", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
-    { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
-    { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
-    { "<leader>gr", function() Snacks.gitbrowse() end, desc = "Git Browse Repo" },
-    { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
-    { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
-    { "<leader>sb", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
-    { "<leader>sc", function() Snacks.picker.commands() end, desc = "Search Commands" },
-    { "<leader>sd", function() Snacks.picker.diagnostics_buffer() end, desc = "Search Buffer Diagnostics" },
-    { "<leader>sD", function() Snacks.picker.diagnostics() end, desc = "Search All Diagnostics" },
-    { "<leader>sf", function() Snacks.picker.files({follow = true, hidden = true}) end, desc = "Search Files" },
-    { "<leader>sg", function() Snacks.picker.grep({follow = true, hidden = true}) end, desc = "Grep" },
-    { "<leader>sh", function() Snacks.picker.help() end, desc = "Search Help" },
-    { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Search Keymaps" },
-    { "<leader>sm", function() Snacks.picker.marks() end, desc = "Search Marks" },
-    { "<leader>sM", function() Snacks.picker.man() end, desc = "Search Man Pages" },
-    { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume Previous Search" },
-    { "<leader>sv", function() Snacks.picker.files({cwd = vim.fn.stdpath("config")}) end, desc = "Search Config Files" },
-    { "<leader>sw", function() Snacks.picker.grep_word({follow = true, hidden = true}) end, desc = "Grep Word or Visual Selection" },
-    { "<leader>sx", function() Snacks.picker.command_history() end, desc = "Search Command History" },
-    {
-      "<leader>N",
-      desc = "Neovim News",
-      function()
-        Snacks.win({
-          file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-          width = 0.6,
-          height = 0.6,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = "yes",
-            statuscolumn = " ",
-            conceallevel = 3,
-          },
-        })
-      end,
-    }
   },
   init = function()
     vim.api.nvim_create_autocmd("User", {
