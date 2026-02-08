919e64197a9aae66d0d6b44a6b3d787b234f89c9
Thiru Thirunavukarasu <thiru0130@gmail.com>
2026-02-08 17:16:24 -0500

    nvim - git keymaps tweaks

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -3,9 +3,9 @@ return {
   cond = not vim.opt.diff:get(),
   lazy = false,
   keys = {
-    { '<leader>gdb', '<CMD>DiffviewFileHistory<CR>', desc = 'Git DiffView branch' },
+    { '<leader>gb', '<CMD>DiffviewFileHistory<CR>', desc = 'Git DiffView branch' },
     { '<leader>gdd', ':DiffviewOpen ', desc = 'Git DiffView open...' },
     { '<leader>gdf', '<CMD>DiffviewFileHistory %<CR>', desc = 'Git DiffView current file' },
-    { '<leader>gds', ':DiffviewOpen HEAD<CR>', desc = 'Git DiffView status' },
+    { '<leader>gs', ':DiffviewOpen HEAD<CR>', desc = 'Git DiffView status' },
   }
 }
