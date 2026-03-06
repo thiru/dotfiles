aa4c46b9aee7b2533e52a0eb525cf332dce39ff2
Thiru Thirunavukarasu <thiru0130@gmail.com>
2026-03-05 22:25:27 -0500

    nvim - nvtmux - hack to fix terminal colour issue

--- .config/nvim/lua/plugins/nvtmux.lua
+++ .config/nvim/lua/plugins/nvtmux.lua
@@ -9,6 +9,10 @@ return {
   opts = {
     auto_start = u.nvtmux_auto_started(),
     colorscheme = 'catppuccin-mocha',
+    -- HACK: terminal colours aren't set correctly without this
+    on_before_term_created = function()
+      vim.cmd('set background=dark')
+    end,
     on_tab_changed = function(is_term_tab)
       if vim.g.neovide then
         if is_term_tab then
@@ -16,6 +20,8 @@ return {
             vim.g.neovide_opacity = neovide.terminal_opacity_override
           end
         else
+          -- NOTE: undo terminal hack above (on_before_term_created)
+          vim.cmd('set background=light')
           if vim.g.neovide_opacity ~= neovide.opacity_default then
             vim.g.neovide_opacity = neovide.opacity_default
           end
