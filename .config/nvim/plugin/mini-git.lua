d0054a521ed857ec63f5f9e15458c91fefa723be
Thiru Thirunavukarasu <thiru0130@gmail.com>
2026-04-12 16:46:22 -0400

    nvim - mini.git - trying it out for first time

---
+++ .config/nvim/plugin/mini-git.lua
@@ -0,0 +1,10 @@
+local p = require('my.packin')
+
+p.add{
+  src = 'https://github.com/nvim-mini/mini-git',
+  name = 'mini.git',
+  opts = {},
+  after_load = function()
+    vim.keymap.set({'n', 'v'}, '<C-m>', ':Git ', {desc = 'mini-git prompt'})
+  end
+}
