c6d23ea0eeda7987b696d5c4f5c03cccdaf743d2
Thiru Thirunavukarasu <thiru0130@gmail.com>
2026-04-12 00:23:20 -0400

    nvim - mini.tabline - make tab pages section more visible

--- .config/nvim/plugin/mini-tabline.lua
+++ .config/nvim/plugin/mini-tabline.lua
@@ -1,8 +1,20 @@
 local p = require('my.packin')
 
+--- Make the highlight group for the tab pages section more visible.
+local function set_tabpage_hl()
+  vim.api.nvim_set_hl(0, 'MiniTablineTabpagesection', { link = 'TabLineSel' })
+  vim.api.nvim_create_autocmd('ColorScheme', {
+    pattern = '*',
+    callback = function()
+      vim.api.nvim_set_hl(0, 'MiniTablineTabpagesection', { link = 'TabLineSel' })
+    end,
+  })
+end
+
 p.add{
   src = 'https://github.com/nvim-mini/mini.tabline',
   opts = {
     tabpage_section = 'right',
-  }
+  },
+  after_load = set_tabpage_hl
 }
