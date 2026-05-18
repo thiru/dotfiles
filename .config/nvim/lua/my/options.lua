720a8cb283a928c71b96d53128b2f57673863c1c
Thiru Thirunavukarasu <thiru0130@gmail.com>
2026-05-17 22:36:52 -0400

    nvim - fix - deprecation warning

--- .config/nvim/lua/my/options.lua
+++ .config/nvim/lua/my/options.lua
@@ -11,7 +11,7 @@ local function highlight_on_yank()
   local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
   vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function()
-      vim.highlight.on_yank()
+      vim.hl.on_yank()
     end,
     group = highlight_group,
     pattern = '*',
