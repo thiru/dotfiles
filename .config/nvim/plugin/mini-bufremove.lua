c583152b7e91a6daa069f9024fa71973ee721d15
Thiru Thirunavukarasu <thiru0130@gmail.com>
2026-07-25 17:33:27 -0400

    nvim - replace buf wipeoutwith buf delete

--- .config/nvim/plugin/mini-bufremove.lua
+++ .config/nvim/plugin/mini-bufremove.lua
@@ -8,7 +8,7 @@ p.add{
     silent = true,
   },
   after_load = function(plugin)
-    vim.keymap.set('n', '<leader>d', plugin.wipeout, {desc = 'Delete buffer'})
+    vim.keymap.set('n', '<leader>d', plugin.delete, {desc = 'Delete buffer'})
     vim.keymap.set('n', '<leader>D',
       function() plugin.wipeout(0, true) end,
       {desc = 'Delete buffer (ignore unsaved changes)'})
