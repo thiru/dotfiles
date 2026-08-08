f473c26152cb5e793de70314fb5f877b98d53524
Thiru Thirunavukarasu <thiru0130@gmail.com>
2026-08-08 15:31:13 -0400

    nvim - mini.statusline - simplify components and ui tweaks

--- .config/nvim/plugin/mini-statusline.lua
+++ .config/nvim/plugin/mini-statusline.lua
@@ -17,24 +17,23 @@ local plugin = require('mini.statusline')
 plugin.setup({
   content = {
     active = function()
-      local _, mode_hl  = plugin.section_mode({ trunc_width = 120 })
-      local workspaces  = tnvws.statusline_text()
-      local cwd         = u.get_cwd()
-      local parent_dir  = vim.bo.buftype == 'terminal' and '' or u.get_file_parent()
-      local git         = vim.bo.buftype == 'terminal' and term_branch() or plugin.section_git({ trunc_width = 40 })
-      local diff        = plugin.section_diff({ trunc_width = 75 })
-      local diagnostics = plugin.section_diagnostics({ trunc_width = 75 })
-      local lsp         = plugin.section_lsp({ trunc_width = 75 })
-      local location    = plugin.section_location({ trunc_width = 75 })
-      local search      = plugin.section_searchcount({ trunc_width = 75 })
+      local _, mode_hl = plugin.section_mode({ trunc_width = 120 })
+      local workspaces = tnvws.statusline_text()
+      local cwd        = u.get_cwd()
+      local parent_dir = vim.bo.buftype == 'terminal' and '' or u.get_file_parent()
+      local git        = vim.bo.buftype == 'terminal' and term_branch() or plugin.section_git({ trunc_width = 40 })
+      local location   = '%{printf("%d|%d|%d%%", col("."), line("."), float2nr(100.0 * line(".") / line("$")))}'
+      local search     = plugin.section_searchcount({ trunc_width = 75 })
 
       return plugin.combine_groups({
-        { hl = mode_hl,                  strings = {workspaces} },
+        { hl = mode_hl, strings = {workspaces} },
         '%<', -- Mark general truncate point
-        { hl = 'MiniStatuslineFilename', strings = {cwd, parent_dir} },
+        { hl = 'TermCursor', strings = {cwd} },
+        { hl = 'MiniStatuslineFileinfo', strings = {parent_dir} },
         '%=', -- End left alignment
-        { hl = 'FloatBorder',            strings = {diagnostics, lsp, diff, search, location} },
-        { hl = mode_hl,                  strings = {git} },
+        { hl = 'Search', strings = {search} },
+        { hl = 'FloatBorder', strings = {location} },
+        { hl = mode_hl, strings = {git} },
       })
     end
   }
