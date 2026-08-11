fbbc0816b71a4650730378dc55ca4d80c5f59190
Thiru Thirunavukarasu <thiru0130@gmail.com>
2026-08-11 12:25:09 -0400

    nvim - hide cmdline (again)

--- .config/nvim/lua/mine/options.lua
+++ .config/nvim/lua/mine/options.lua
@@ -132,6 +132,9 @@ vim.opt.nrformats = 'alpha,blank'
 -- Enable mouse in all modes
 vim.opt.mouse = 'a'
 
+-- Hide cmdline by default
+vim.opt.cmdheight = 0
+
 -- Use one global status line (not per window)
 vim.opt.laststatus = 3
 
